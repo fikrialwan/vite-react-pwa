f38a712db3fccf78097da52b88c8d7c05c2a296d
fikrialwan <fikriar93@gmail.com>
2024-06-09 21:19:57 +0700

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,49 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: "script-defer",
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "ViteReactPWA",
+        short_name: "Explore PWA using vite react",
+        theme_color: "#1a1a1a",
+        icons: [
+          {
+            src: "favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "favicon-79x70.png",
+            sizes: "70x70",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
 });
